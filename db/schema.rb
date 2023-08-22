c98ccfc8553611ecca25a86d69d3f172b3569852
EC2 Default User <141287848+Shunta126@users.noreply.github.com>
2023-08-22 07:24:02 +0000

    23まで

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_22_044112) do
+ActiveRecord::Schema.define(version: 2023_08_22_072121) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2023_08_22_044112) do
     t.datetime "updated_at", precision: 6, null: false
     t.text "title"
     t.text "body"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
