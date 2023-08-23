7f0ef9df55bccb702bf97be3187168ca715d0d6f
EC2 Default User <141287848+Shunta126@users.noreply.github.com>
2023-08-23 09:35:52 +0000

    45まで

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,35 @@
 class UsersController < ApplicationController
   def index
+     @user = User.find(current_user.id)
+      @book = Book.new
+     @users = User.all
+  end
+
+   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
   end
 
   def show
+    @users = User.find(params[:id])
+    @book = Book.new
+    @books = Book.all
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction)
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :body, :user_id)
+  end
+
 
 end
