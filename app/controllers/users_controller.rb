a2057e757617d3321a64068d4f28058369f7107a
EC2 Default User <141287848+Shunta126@users.noreply.github.com>
2023-08-24 14:54:37 +0000

    レイアウト前

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    flash[:notice] = "Welcome! You have signed up successfully."
+    flash[:notice] = "You have created book successfully."
     redirect_to book_path(@book.id)
   end
 
@@ -25,6 +25,7 @@ class UsersController < ApplicationController
 
   def update
     users =  User.find(params[:id])
+    @post_image = PostImage.new
     users.update(user_params)
     flash[:notice] = "You have updated user successfully."
     redirect_to user_path(users.id)
@@ -32,11 +33,15 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:name, :introduction)
+    params.require(:user).permit(:name, :introduction )
   end
 
   def book_params
     params.require(:book).permit(:title, :body, :user_id)
+  end
+
+  def post_image_params
+     params.require(:post_image).permit(:shop_name, :image, :caption)
   end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_22_072121) do
+ActiveRecord::Schema.define(version: 2023_08_24_091144) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -46,6 +46,14 @@ ActiveRecord::Schema.define(version: 2023_08_22_072121) do
     t.text "title"
     t.text "body"
     t.integer "user_id"
+  end
+
+  create_table "post_images", force: :cascade do |t|
+    t.string "shop_name"
+    t.text "caption"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
